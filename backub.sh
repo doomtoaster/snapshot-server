33051a7d88fba3643a055ed48b6890b912b6d7d9
root <root@localhost.localdomain>
2024-01-05 12:39:41 +0000

    hilfe warum klappt nicht

--- backub.sh
+++ backub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /bin/echo "cron works" >> /home/kai/server/world/file
 
-git add .
-git commit -m "hilfe warum klappt nicht"
-git push
+git -C /home/kai/server/world/ add .
+git -C /home/kai/server/world/ commit -m "hilfe warum klappt nicht"
+git -C /home/kai/server/world/ push
