a147478594bb76d15dcbe51e59982c61b10fe643
root <root@localhost.localdomain>
2024-01-05 12:50:01 +0000

    Fri Jan  5 12:50:01 UTC 2024

--- backub.sh
+++ backub.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+date=$(date)
 /bin/echo "cron works" >> /home/kai/server/world/file
 
 git -C /home/kai/server/world/ add .
-git -C /home/kai/server/world/ commit -m "hilfe warum klappt nicht"
+git -C /home/kai/server/world/ commit -m "$date"
 git -C /home/kai/server/world/ push
